488e06a885c045199b8c1d22dff828ca25a41161
GOURI SANKAR S <87740589+Gouri124@users.noreply.github.com>
2026-01-21 17:39:36 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import datetime
+
+name = "Gourisankar S"
+current_time = datetime.datetime.now().strftime("04:48:34")
+
+print(f"Hello {name}, right now the time is {current_time}")
